e843de55bef0f54a5fb2f62a63085f2afa4b7c5c
ziad <ziadelsaadany73@gmail.com>
2023-03-17 20:15:33 +0200

    add serverException to solve error add getNowPlaying in dataSource

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,20 @@
+import 'package:clean_architecture_movies_app/core/network/error_message_model.dart';
+
+// for server
+class ServerException implements Exception {
+  final ErrorMessageModel errorMessageModel ;
+
+  ServerException( {
+    required this.errorMessageModel
+});
+
+}
+
+// for local
+class LocalDatabaseException implements Exception {
+  final String message;
+
+  const LocalDatabaseException({
+    required this.message,
+  });
+}
--- lib/movies/data/data_source/movie_remote_data_source.dart
+++ lib/movies/data/data_source/movie_remote_data_source.dart
@@ -1,32 +1,33 @@
+import 'package:clean_architecture_movies_app/core/error/exceptions.dart';
 import 'package:clean_architecture_movies_app/core/utls/app_constant.dart';
 import 'package:clean_architecture_movies_app/movies/data/models/movies_model.dart';
 import 'package:dio/dio.dart';
 
-class BaseMovieRemoteDataSource {
+import '../../../core/network/error_message_model.dart';
+
+abstract class BaseMovieRemoteDataSource {
+  Future<List<MovieModel>?> getNowPlayingMovies();
+}
+
+class MovieRemoteDataSource extends BaseMovieRemoteDataSource {
 
 
   //  methods --> from useCases
-
-Future<List<MovieModel>>  getNowPlaying( )  async{
-
-    try{
-  final response =     await Dio().get(
+@override
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await Dio().get(
         "${AppConstant.baseUrl}/movies/now_playing?${AppConstant.apiKey}"
-      );
-      if(response.statusCode == 200 ) {
-     List resData =  response.data["results"];
-        return
-          List<MovieModel>.from(resData) .map((e) =>
-          MovieModel.fromJson(e as Map<String, dynamic>)
-          ).toList();
-
-      }
-      else{
-      }
-    }catch(e){
-
+    );
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from((response.data["results"] as List).map(
+            (e) => MovieModel.fromJson(e),
+      ));
     }
-
+    else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
   }
 
 
