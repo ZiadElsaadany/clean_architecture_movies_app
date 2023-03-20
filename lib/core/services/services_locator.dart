ff85eb91318d07268468127d897bc658b0ce5cd2
ziad <ziadelsaadany73@gmail.com>
2023-03-20 18:53:05 +0200

    33. Service locator difference between register lazy singleton and register factory

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -2,11 +2,18 @@ import 'package:clean_architecture_movies_app/movies/data/data_source/movie_remo
 import 'package:clean_architecture_movies_app/movies/data/repository/movies_repository.dart';
 import 'package:clean_architecture_movies_app/movies/domain/repository/base_movies_repo.dart';
 import 'package:clean_architecture_movies_app/movies/domain/usecases/get_now_playing.dart';
+import 'package:clean_architecture_movies_app/movies/presentation/controller/movies_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 class ServiceLocator {
   void init  ( ){
+    ///Bloc
+    ///
+    /// new object from MovieBloc
+    sl.registerFactory(() => MoviesBloc(sl()));
+
+
     /// Use cases
 
     sl.registerLazySingleton(() =>GetNowPlayingMoviesUseCase(
